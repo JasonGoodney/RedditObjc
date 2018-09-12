d893382478fee842506a6f7123e8a05f7d6f19c8
Jason Goodney <Jason.Goodney@gmail.com>
2018-09-12 10:15:10 -0600

    Initial commit

---
+++ RedditObjc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RedditObjc
+//
+//  Created by Jason Goodney on 9/12/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
